a43d84697313e56ec6a2138111270caf08524e7e
Brianbramasta <Brianaldybramasta@gmail.com>
2025-03-25 01:44:36 +0700

    root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,13 @@
 'use client';
-import { useAuth } from '@/contexts/AuthContext';
-import Link from 'next/link';
-import { CustomButton } from '@/components/CustomButton/CustomButton';
-import styles from './page.module.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function DashboardPage() {
-  const { user, logout } = useAuth();
+export default function Home() {
+  const router = useRouter();
 
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Dashboard</h1>
-      
-      {user && (
-        <div className={styles.content}>
-          <p className={styles.welcome}>Welcome {user.email}</p>
-          
-          <div className={styles.buttonGroup}>
-            <Link href="/task_management" className={styles.link}>
-              <CustomButton variant="primary">
-                Task Management
-              </CustomButton>
-            </Link>
-            
-            <Link href="/toko_online" className={styles.link}>
-              <CustomButton variant="primary">
-                Toko Online
-              </CustomButton>
-            </Link>
-            
-            
-          </div>
-          <div className={styles.buttonGroup} style={{marginTop:'20px'}}>
-            <CustomButton 
-                onClick={logout} 
-                variant="secondary"
-            >
-                Logout
-            </CustomButton>
-            </div>
-          
-          
-        </div>
-      )}
-      
-    
-      
-    </div>
-  );
+  useEffect(() => {
+    router.push('/login');
+  }, [router]);
+
+  return null;
 }
